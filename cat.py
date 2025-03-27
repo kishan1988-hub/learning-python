4cc8014887f1fb5a9f3125e95b80ac45288dd9ad
kishan1988-hub <kishan1988@gmail.com>
2025-03-27 22:58:04 +0530

    for loop

--- cat.py
+++ cat.py
@@ -1,5 +1,18 @@
-i = 1
+# i = 0
 
-while i  <=3:
+# while i  <=3:
+#     print("Meow")
+#     i+=1
+
+# for i in [0,1,2]: # to be looped thru the list of values
+#     print("Meow")
+
+# for i in range(3):
+#     print("Meow")
+
+n = int(input("Pass the value to iterate the word meow - "))
+
+for i in range(n):
     print("Meow")
-    i+=1
+
+print("Meoww\n" * 3, end="")
