f55fa6264d02315159b30a5e72e4e3ee056faa35
kishan1988-hub <kishan1988@gmail.com>
2025-03-27 23:20:46 +0530

    updated hogwarts for combined range and len on list

--- hogwarts.py
+++ hogwarts.py
@@ -2,3 +2,6 @@ students = ["Hermonie","Harry","Ron"]
 
 for student in students:
     print(student)
+
+for i in range(len(students)):  # combining range on list using len function
+    print(students[i])
